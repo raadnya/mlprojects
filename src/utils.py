2e4dd6b24a642b5f7b4399a2f32861efd41afc6f
Raadnya Apte <raadnya@gmail.com>
2024-10-02 14:41:24 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -18,23 +19,30 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     try:
         report = {}
-        for model_name, model in models.items():
-            # Fit the model
-            model.fit(X_train, y_train)
 
-            # Predict on train and test data
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+        for i in range(len(list(models))):
+            
+            model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
 
             # Calculate R^2 score
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
             # Store the test model score in the report
-            report[model_name] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
